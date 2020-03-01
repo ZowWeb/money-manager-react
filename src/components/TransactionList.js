86da23e99dc10e10e9e07bbfc8391880ec07a77e
ZowWeb <wrongzoayb@gmail.com>
2020-03-01 20:50:12 +0530

    Pull out transactions from GlobalContext using useContext and map each to Transaction component

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Transaction } from "./Transaction";
+
+import { GlobalContext } from "../context/GlobalState";
 
 export const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
   return (
     <>
       <h3>History</h3>
-      <ul class="list">
-        <li class="minus">
-          Cash <span>-$400</span>
-          <button class="delete-btn">x</button>
-        </li>
+      <ul className="list">
+        {transactions.map(transaction => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))}
       </ul>
     </>
   );
