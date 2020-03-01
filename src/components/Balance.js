e9525a07c80bbe26bd6643ed461fe6eaf54decc3
ZowWeb <wrongzoayb@gmail.com>
2020-03-01 21:14:48 +0530

    Calculate Balance, Income and Expenses and assign them to resp components to show real time data

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const Balance = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  // Update the balance
+  const amounts = transactions.map(transaction => transaction.amount);
+  const total = amounts.reduce((acc, amount) => (acc += amount), 0).toFixed(2);
+
   return (
     <>
       <h4>Your Balance</h4>
-      <h1>$0.00</h1>
+      <h1>&#8377;{total}</h1>
     </>
   );
 };
--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const IncomeExpenses = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  // Update the income and expense
+  const amounts = transactions.map(transaction => transaction.amount);
+  const income = amounts
+    .filter(amount => amount > 0)
+    .reduce((acc, amount) => (acc += amount), 0)
+    .toFixed(2);
+
+  const expense = (
+    amounts
+      .filter(amount => amount < 0)
+      .reduce((acc, amount) => (acc += amount), 0) * -1
+  ).toFixed(2);
+
   return (
     <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
-        <p className="money plus">+$0.00</p>
+        <p className="money plus">&#8377;{income}</p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p className="money minus">-$0.00</p>
+        <p className="money minus">&#8377;{expense}</p>
       </div>
     </div>
   );
