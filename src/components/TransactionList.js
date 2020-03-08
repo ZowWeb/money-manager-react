cd742d0a1ca13ac2a6bbadae5c68691bda53e045
Zohaib Khan <38921817+ZowWeb@users.noreply.github.com>
2020-03-08 13:35:56 +0530

    Revert "Bug fix"

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,22 +1,17 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { Transaction } from "./Transaction";
 
 import { GlobalContext } from "../context/GlobalState";
 
 export const TransactionList = () => {
-  const { transactions, getTransactions } = useContext(GlobalContext);
-
-  useEffect(() => {
-    getTransactions();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const { transactions } = useContext(GlobalContext);
 
   return (
     <>
       <h3>History</h3>
       <ul className="list">
         {transactions.map(transaction => (
-          <Transaction key={transaction._id} transaction={transaction} />
+          <Transaction key={transaction.id} transaction={transaction} />
         ))}
       </ul>
     </>
--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,27 +1,16 @@
 export default (state, action) => {
   switch (action.type) {
-    case "GET_TRANSACTIONS":
-      return {
-        ...state,
-        loading: false,
-        transactions: action.payload
-      };
     case "DELETE_TRANSACTION":
       return {
         ...state,
         transactions: state.transactions.filter(
-          transaction => transaction._id !== action.payload
+          transaction => transaction.id !== action.payload
         )
       };
     case "ADD_TRANSACTION":
       return {
         ...state,
-        transactions: [...state.transactions, action.payload]
-      };
-    case "TRANSACTION_ERROR":
-      return {
-        ...state,
-        error: action.payload
+        transactions: [action.payload, ...state.transactions]
       };
     default:
       return state;
