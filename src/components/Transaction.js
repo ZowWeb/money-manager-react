cd574b9f019739516df5c98c4d41fdf90c9738a7
ZowWeb <wrongzoayb@gmail.com>
2020-04-05 17:58:19 +0530

    :lipstick: Implement Material Paper component with theme spacing and palette

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -2,24 +2,45 @@ import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
 import { numberWithCommas } from "../utils/format";
 
+import { Grid, Paper, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  item: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: theme.spacing(2),
+    margin: `${theme.spacing(1)}px 0`,
+  },
+  red: theme.palette.error.main,
+}));
+
 export const Transaction = ({ transaction }) => {
   const { deleteTransaction } = useContext(GlobalContext);
+  const classes = useStyles();
 
   // Get sign
   const sign = transaction.amount < 0 ? "-" : "+";
 
   return (
-    <li className={transaction.amount < 0 ? "minus" : "plus"}>
-      {transaction.text}{" "}
-      <span>
-        {sign} &#8377;{numberWithCommas(Math.abs(transaction.amount))}
-      </span>
-      <button
-        className="delete-btn"
-        onClick={() => deleteTransaction(transaction._id)}
+    <Grid container xs={12}>
+      <Paper
+        className={classes.item + (transaction.amount < 0 ? " minus" : " plus")}
       >
-        x
-      </button>
-    </li>
+        <span>{transaction.text}</span>
+        <span>
+          {sign} &#8377;{numberWithCommas(Math.abs(transaction.amount))}
+          <Button
+            color="secondary"
+            onClick={() => deleteTransaction(transaction._id)}
+          >
+            del
+          </Button>
+        </span>
+      </Paper>
+    </Grid>
   );
 };
