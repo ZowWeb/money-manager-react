06e046db818cf19ff3953a45f9b9c668d7fa867b
ZowWeb <wrongzoayb@gmail.com>
2020-04-06 11:56:18 +0530

    :lipstick: Add Paper

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -2,24 +2,26 @@ import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
 import { numberWithCommas } from "../utils/format";
 
+import { Paper } from "@material-ui/core";
+
 export const IncomeExpenses = () => {
   const { transactions, loading } = useContext(GlobalContext);
 
   // Update the income and expense
-  const amounts = transactions.map(transaction => transaction.amount);
+  const amounts = transactions.map((transaction) => transaction.amount);
   const income = amounts
-    .filter(amount => amount > 0)
+    .filter((amount) => amount > 0)
     .reduce((acc, amount) => (acc += amount), 0)
     .toFixed(2);
 
   const expense = (
     amounts
-      .filter(amount => amount < 0)
+      .filter((amount) => amount < 0)
       .reduce((acc, amount) => (acc += amount), 0) * -1
   ).toFixed(2);
 
   return (
-    <div className="inc-exp-container">
+    <Paper className="inc-exp-container" elevation={3}>
       <div>
         <h4>Income</h4>
         <p className={"money plus " + (loading ? "loading" : "")}>
@@ -32,6 +34,6 @@ export const IncomeExpenses = () => {
           &#8377;{numberWithCommas(expense)}
         </p>
       </div>
-    </div>
+    </Paper>
   );
 };
