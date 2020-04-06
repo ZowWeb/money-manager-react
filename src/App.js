d1d1addd05ad522b3e4c01014d98ec46311b8e9e
ZowWeb <wrongzoayb@gmail.com>
2020-04-06 14:11:58 +0530

    :sparkles: Add feature dark mode toggle

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Balance } from "./components/Balance";
 import { IncomeExpenses } from "./components/IncomeExpenses";
 import { TransactionList } from "./components/TransactionList";
@@ -16,61 +16,72 @@ import {
   Typography,
   Toolbar,
   IconButton,
+  Paper,
 } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
+import Brightness4Icon from "@material-ui/icons/Brightness4";
+import Brightness7Icon from "@material-ui/icons/Brightness7";
 import { makeStyles } from "@material-ui/core/styles";
 import theme from "./theme";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    padding: theme.spacing(2),
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
-
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+      padding: theme.spacing(2),
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
   const classes = useStyles();
 
   return (
     <GlobalProvider>
-      <MuiThemeProvider theme={theme}>
-        <CssBaseline />
-        <AppBar position="sticky">
-          <Toolbar>
-            <IconButton
-              edge="start"
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="menu"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.title}>
-              Money Manager App
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Grid container direction="column" className={classes.root}>
-          <Grid item xs={12}></Grid>
-          <Grid item xs={12}>
-            <Balance />
+      <MuiThemeProvider theme={theme(darkMode)}>
+        <Paper>
+          <CssBaseline />
+          <AppBar position="sticky">
+            <Toolbar>
+              <IconButton
+                edge="start"
+                className={classes.menuButton}
+                color="inherit"
+                aria-label="menu"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" className={classes.title}>
+                Money Manager App
+              </Typography>
+              <IconButton
+                color="inherit"
+                onClick={(e) => setDarkMode(!darkMode)}
+              >
+                {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+          <Grid container direction="column" className={classes.root}>
+            <Grid item xs={12}></Grid>
+            <Grid item xs={12}>
+              <Balance />
+            </Grid>
+            <Grid item xs={12}>
+              <IncomeExpenses />
+            </Grid>
+            <Grid item xs={12}>
+              <TransactionList />
+            </Grid>
+            <Grid item xs={12}>
+              <AddTransaction />
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <IncomeExpenses />
-          </Grid>
-          <Grid item xs={12}>
-            <TransactionList />
-          </Grid>
-          <Grid item xs={12}>
-            <AddTransaction />
-          </Grid>
-        </Grid>
+        </Paper>
       </MuiThemeProvider>
     </GlobalProvider>
   );
